52aa9d23477ed6b29d0c11fce3d04dfc590afddd
Benjamin Szeghy <benjamin.szeghy@berkeley.edu>
2026-09-22 21:33:21 -0400

    add natural earth basemap

--- src/ipyd3geo/static/widget.js
+++ src/ipyd3geo/static/widget.js
@@ -1,6 +1,9 @@
 import * as d3 from "https://esm.sh/d3-geo@3";
 
-function render({ model, el }) {
+async function render({ model, el }) {
+  const countries = await fetch(
+    "https://cdn.jsdelivr.net/gh/nvkelso/natural-earth-vector/geojson/ne_110m_admin_0_countries.geojson"
+  ).then((res) => res.json());
   const path = d3.geoPath(d3[model.get("projection")]());
 
   const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
@@ -13,11 +16,18 @@ function render({ model, el }) {
   sphere.setAttribute("stroke", model.get("border") ? "black" : "none");
   svg.appendChild(sphere);
 
+  const naturalEarth = document.createElementNS("http://www.w3.org/2000/svg", "path");
+  naturalEarth.setAttribute("d", path(countries));
+  naturalEarth.setAttribute("fill", "none");
+  naturalEarth.setAttribute("stroke", "black");
+  svg.appendChild(naturalEarth);
+
   if (model.get("graticule")) {
     const graticule = document.createElementNS("http://www.w3.org/2000/svg", "path");
     graticule.setAttribute("d", path(d3.geoGraticule()()));
     graticule.setAttribute("fill", "none");
     graticule.setAttribute("stroke", "lightgray");
+    graticule.setAttribute("opacity", "0.5");
     svg.appendChild(graticule);
   }
 
